8011368df2a041c261c224a7936bee4372f474c9
lawtonhself <lawtonhself@hotmail.com>
2022-07-12 17:55:12 -0400

    Basic start
    Added 2 buttons and a text object. Got 1 point to add every 10 seconds to the points and working on getting buttons to work with onclick method

---
+++ Assets/Scripts/PointsManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointsManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public double points = 0;
+    public Text pointsDisplay;
+
+    int i;
+    int j;
+    int k;
+    int l;
+
+    void Start()
+    {
+        pointsDisplay.text = points.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        timerMethod(5, 1);
+        //timerMethod(10, 1, 2);
+
+
+        pointsDisplay.text = points.ToString() + " Points!";
+
+    }
+
+    //k is the amount of points you wanted added every j seconds
+    public int timerMethod(int j, int k)
+    {
+        if (Time.time > i)
+        {
+            i += j;
+            points += k;
+        }
+
+        return i;
+    }
+
+    //this is another timer method.k is the amount of points you wanted added every j seconds with a modifier of l  
+    public int timerMethod(int j, int k, int l)
+    {
+        if (Time.time > i)
+        {
+            i += (j*(1/l));
+            points += k;
+        }
+
+        return i;
+    }
+
+    public void counting()
+    {
+        points += 1;
+    }
+
+
+}
